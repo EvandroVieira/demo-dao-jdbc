bf4d855e8e24a9bfee25a964135da1708134f773
EvandroVieira <evandro.vieira@elo.com.br>
2024-12-13 20:46:34 -0300

    Department entity class

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,12 +1,13 @@
 package aplicacao;
 
 import db.DB;
+import modelo.entidade.Departamento;
 
 import java.sql.Connection;
 
 public class Principal {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Departamento obj = new Departamento(1, "Books");
+        System.out.println(obj);
     }
 }
