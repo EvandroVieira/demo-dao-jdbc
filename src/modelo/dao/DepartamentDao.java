76f017a456159d1ebb1e5bba59cc7f68163c1517
EvandroVieira <evandro.vieira@elo.com.br>
2024-12-13 21:01:02 -0300

    DepartamentDao and SellerDao interfaces

---
+++ src/modelo/dao/DepartamentDao.java
@@ -0,0 +1,13 @@
+package modelo.dao;
+
+import modelo.entidade.Departament;
+
+import java.util.List;
+
+public interface DepartamentDao {
+    void insert(Departament obj);
+    void update(Departament obj);
+    void deleteById(Integer id);
+    Departament findById(Integer id);
+    List<Departament> findAll();
+}
