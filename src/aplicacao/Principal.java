5a21c8739cc0a529dbe214b7c61cdc1914e25cd4
EvandroVieira <evandro.vieira@elo.com.br>
2024-12-13 21:06:36 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,5 +1,7 @@
 package aplicacao;
 
+import modelo.dao.DaoFactory;
+import modelo.dao.SellerDao;
 import modelo.entidade.Departament;
 import modelo.entidade.Seller;
 
@@ -10,6 +12,8 @@ public class Principal {
         Departament obj = new Departament(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
